be29b0269d7a53b224be52eee302d124fd6f7201
roman.pohribniak <roman.pohribniak@gmail.com>
2023-02-16 13:21:05 +0200

    deploy

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <section className="footer">
-            <div className="footer__content">This page was created in Ukraine by <a href="https://github.com/PohribniakRoman">Roman Pohribniak</a></div>
-        </section>
-    )
-}
